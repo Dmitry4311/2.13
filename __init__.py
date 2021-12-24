60ef02e1bf868fe15535eab1338df539fab650e9
Dmitry4311 <78484409+Dmitry4311@users.noreply.github.com>
2021-12-24 14:36:50 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .func2 import show_commands, add_student, show_list, show_selected
-
-
-__all__ = ['show_commands', 'add_student', 'show_list', 'show_selected']
